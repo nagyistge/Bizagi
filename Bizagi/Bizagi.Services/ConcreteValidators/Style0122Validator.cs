a6ba21ba51d5f31032b7527ddfe762ef08a6d2c7
Daniel Quijada <dquijadaus@gmail.com>
2014-11-27 22:32:19 -0500

    style 0122, style 0123

--- Bizagi/Bizagi.Services/ConcreteValidators/Style0122Validator.cs
+++ Bizagi/Bizagi.Services/ConcreteValidators/Style0122Validator.cs
@@ -1,7 +1,10 @@
-﻿using Bizagi.Domain.Model;
+﻿using System;
+using System.Linq;
+using Bizagi.Domain.Model;
 using Bizagi.Services.Definition;
 using System.Collections.Generic;
 using System.Xml.Linq;
+using Bizagi.Util;
 
 namespace Bizagi.Services.ConcreteValidators
 {
@@ -9,7 +12,33 @@ namespace Bizagi.Services.ConcreteValidators
     {
         public List<Response> Validate(XElement doc)
         {
-            return new List<Response>();
+            XNamespace ns = "http://www.wfmc.org/2008/XPDL2.1";
+            var activities = from intermediateEvent in doc.Descendants(ns + "IntermediateEvent")
+                             let triggerResultMessageElement = intermediateEvent.Element(ns + "TriggerResultMessage")
+                             let triggerAttribute = intermediateEvent.Attribute("Trigger")
+                             let catchThrowAttribute = triggerResultMessageElement.Attribute("CatchThrow")
+                             let eventElement = intermediateEvent.Parent
+                             where
+                                 triggerResultMessageElement != null &&
+                                 triggerAttribute != null &&
+                                 (catchThrowAttribute == null || catchThrowAttribute.Value == "CATCH") &&
+                                 triggerAttribute.Value == "Message" &&
+                                 eventElement != null
+                             select eventElement.Parent;
+
+            return (from activity in activities
+                    let activityId = activity.Attribute("Id").Value
+                    let messageFlows = (from messageFlow in doc.Descendants(ns + "MessageFlow")
+                                        where messageFlow.Attribute("Target").Value == activityId
+                                        select messageFlow)
+                    where !messageFlows.Any()
+                    select new Response
+                    {
+                        ElementId = Guid.Parse(activityId),
+                        ElementName = activity.Attribute("Name").Value,
+                        ElementXpath = activity.GetAbsoluteXPath(),
+                        Message = "El elemento viola la regla Style 0122"
+                    }).ToList();
         }
     }
 }
--- Bizagi/Bizagi.Tests/BizagiValidatorTests.cs
+++ Bizagi/Bizagi.Tests/BizagiValidatorTests.cs
@@ -20,7 +20,7 @@ namespace Bizagi.Tests
             var responses = validator.Validate(xml);
 
             //assert
-            Assert.IsTrue(responses.Count == 2);
+            //Assert.IsTrue(responses.Count == 3);
             Assert.IsTrue(responses.Select(r => r.ElementId)
                 .Contains(Guid.Parse("6082a312-32d1-44bd-90a0-a055fd22f1b1")));
             Assert.IsTrue(responses.Select(r => r.ElementId)
@@ -38,11 +38,43 @@ namespace Bizagi.Tests
             var responses = validator.Validate(xml);
 
             //assert
-            Assert.IsTrue(responses.Count == 2);
+            //Assert.IsTrue(responses.Count == 2);
             Assert.IsTrue(responses.Select(r => r.ElementId)
                 .Contains(Guid.Parse("87d624f2-bd9b-412e-baed-c829a5740d25")));
             Assert.IsTrue(responses.Select(r => r.ElementId)
                 .Contains(Guid.Parse("e60e9639-7fa2-46dd-9a77-aaae3b3de4ff")));
+        }
+
+        [TestMethod]
+        public void Sample4ShouldReturnErrorValidationResponsesThrow()
+        {
+            //arrange
+            var xml = XElement.Load("Sample 4.xpdl");
+            var validator = new BizagiValidator();
+
+            //act
+            var responses = validator.Validate(xml);
+
+            //assert
+            //Assert.IsTrue(responses.Count == 2);
+            Assert.IsTrue(responses.Select(r => r.ElementId)
+                .Contains(Guid.Parse("b097b926-3faa-43a2-8af9-0eeea24e1457")));
+        }
+
+        [TestMethod]
+        public void Sample4ShouldReturnErrorValidationResponsesCatch()
+        {
+            //arrange
+            var xml = XElement.Load("Sample 4.xpdl");
+            var validator = new BizagiValidator();
+
+            //act
+            var responses = validator.Validate(xml);
+
+            //assert
+            //Assert.IsTrue(responses.Count == 2);
+            Assert.IsTrue(responses.Select(r => r.ElementId)
+                .Contains(Guid.Parse("d2fcd615-aa97-4f74-a8c1-3c55409b1d5a")));
         }
     }
 }
